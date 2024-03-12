125310924ce4a615865505813a709e87f9b22e76
natebobsee <nate.bob.see@gmail.com>
2024-03-12 13:38:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//multipy two numbers
+//if two numbers are input return result.
+//if only one number is input return error
+//if something other than a number is input return error
+
+//COncatOdds
+//are only two lists of numbers provided, error if anything else
+//does the output contain odds from both lists
+//does the output delete any repeating numbers
+//does the output ignore extranious cases
+//is the output put in assending order 
+//is the output only odd  numbers.
+//does the output include the odds from both inputs
+
+//shopping cart
+//Is the user allowed to check out as both a user and a guest.
+//is the guest user prompted to make an account
+//the user is prompted if the cart is empty.
+//check that the user is ready to check out or wishes to add more.
+//the user is given the final price including shipping.
+//the user is prompted to select prefferred shipping method after given a list of alternatives.
+//the user is given a final receipt with shipping date and a total.
+//the user is given a list of similar items to paruse.
+//the user is given a link to reorder the same items.
+//does the cart remember the order if the user logs out before buying
+//
